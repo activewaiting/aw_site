7405731c97a9450cb236ac36e850aa2b4886dbce
Gmasek <127222721+Gmasek@users.noreply.github.com>
2026-06-30 14:37:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/aw_site/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
